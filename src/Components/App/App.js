045656cc10551d4d2f5c64e1e5b55f670fbacdca
level7dc <6236517+level7dc@users.noreply.github.com>
2020-06-18 13:49:27 +0100

    Includes component import/export structure

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
-// import { SearchBar } from '../SearchBar/SearchBar';
-// import { SearchResults } from '../SearchResults/SearchResults';
-// import { PlayList } from '../PlayList/PlayList';
+import { SearchBar } from '../SearchBar/SearchBar';
+import { SearchResults } from '../SearchResults/SearchResults';
+import { PlayList } from '../PlayList/PlayList';
 
 function App() {
   return (
     <div>
     <h1>Ja<span className="highlight">mmm</span>ing</h1>
     <div className="App">
-      {/* <SearchBar /> */}
+      <SearchBar />
       <div className="App-playlist">
-        {/* <SearchResults /> */}
-        {/* <PlayList /> */}
+        <SearchResults />
+        <PlayList />
       </div>
     </div>
   </div>
