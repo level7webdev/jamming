8c1d29c8952b97fae88dedcb697d16c459288e9b
level7dc <6236517+level7dc@users.noreply.github.com>
2020-06-20 15:02:53 +0100

    Search bar set state with search term

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -26,10 +26,15 @@ class App extends React.Component {
       playlistName: "Newer Playlist",
       playlistTracks: playlistTracks,
     };
+    this.search = this.search.bind(this);
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
+  }
+
+  search(searchTerm) {
+    console.log(searchTerm);
   }
 
   addTrack(track) {
@@ -63,7 +68,7 @@ class App extends React.Component {
           Ja<span className="highlight">mmm</span>ing
         </h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
             <SearchResults
               searchResults={this.state.searchResults}
